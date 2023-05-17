fca2bd43e5977d5972680bfb7e4348eaadf4b08c
Krypto2405 <larssonrecords@googlemail.com>
2023-05-17 17:22:13 +0200

    modified:   Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
-print()
+def calculate_square(number):
+    square = number ** 2
+    return square
+
+def main():
+    number = float(input("Geben Sie eine Zahl ein: "))
+    result = calculate_square(number)
+    print("Das Quadrat von", number, "ist", result)
+
+if __name__ == "__main__":
+    main()
