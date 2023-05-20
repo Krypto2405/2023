b64442306aeecbfe3a8f7e7e0de8df30b35b606f
Krypto2405 <larssonrecords@googlemail.com>
2023-05-21 00:15:24 +0200

    Co-authored-by: Daniel Dik <CrytonicD@users.noreply.github.com>

---
+++ Projekte/d.py
@@ -0,0 +1,43 @@
+'''Vorkenntnisse: Kapitel 1.
+Erstelle ein Programm, das deinen Vorname, Nachname und dein Geburtsdatum auf der Konsole ausgibt.'''
+'''
+import time
+
+x = str input("Wie ist dein Vorname?")
+y = str input("Wie ist dein Nachname?")
+z = float input("Geburtsdatum?")
+
+print(x, y, z)
+'''
+'''
+int = integer = 17.13
+float = 17
+str = string = "ABC"
+
+
+s = 17
+c = 3
+print(s + c)
+'''
+'''
+Schreibe ein Programm, das Temperaturen in verschiedene Skalensystemen ineinander umwandelt. Das Programm soll zu Beginn eine Auswahl mit den verschiedenen Möglichkeiten anbieten:
+
+
+(1) Umrechnung von Celsius nach Kelvin
+(2) Umrechnung von Celsius nach Fahrenheit
+(3) Umrechnung von Kelvin nach Celsius
+(4) Umrechnung von Kelvin nach Fahrenheit
+(5) Umrechnung von Fahrenheit nach Celsius
+(6) Umrechnung von Fahrenheit nach Kelvin
+
+Bemerkung
+
+Dies kann dir sicher helfen.
+
+    Celsius = 5/9 * (Fahrenheit - 32).
+
+    Celsius = Kelvin - 273.15.
+
+    Die tiefste mögliche Temperatur ist den absoluten Nullpunkt 0K.
+    '''
+
---
+++ Projekte/k.py
@@ -0,0 +1,55 @@
+'''Vorkenntnisse: Kapitel 1.
+Erstelle ein Programm, das deinen Vorname, Nachname und dein Geburtsdatum auf der Konsole ausgibt.'''
+'''
+import time
+
+x = str input("Wie ist dein Vorname?")
+y = str input("Wie ist dein Nachname?")
+z = float input("Geburtsdatum?")
+
+print(x, y, z)
+'''
+'''
+x = 17.13
+y = 17
+z = "ABC"
+
+
+s = 17
+c = 3
+print(x + y)
+'''
+
+'''
+Schreibe ein Programm, das Temperaturen in verschiedene Skalensystemen ineinander umwandelt. Das Programm soll zu Beginn eine Auswahl mit den verschiedenen Möglichkeiten anbieten:
+
+
+Bemerkung
+
+Dies kann dir sicher helfen.
+
+    Celsius = 5/9 * (Fahrenheit - 32).
+
+    Celsius = Kelvin - 273.15.
+
+    Die tiefste mögliche Temperatur ist den absoluten Nullpunkt 0K.
+    '''
+
+print("""
+(1) Umrechnung von Celsius nach Kelvin
+(2) Umrechnung von Celsius nach Fahrenheit
+(3) Umrechnung von Kelvin nach Celsius
+(4) Umrechnung von Kelvin nach Fahrenheit
+(5) Umrechnung von Fahrenheit nach Celsius
+(6) Umrechnung von Fahrenheit nach Kelvin
+""")
+
+e = input("Wählen sie zwischen 1 - 6:\n")
+
+
+1 = e + 273.15
+2 = (5/9 * (e - 32))
+3 =
+4 =
+5 =
+6 =
