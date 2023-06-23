5618fd51c3c514869fa019d8e15696c83cd4aa69
CrytonicD <dik.daniel@web.de>
2023-06-23 20:45:48 +0200

    new file:   Projekte/Daniel/wetter.py

---
+++ Projekte/Daniel/wetter.py
@@ -0,0 +1,12 @@
+"Ist das Wetter gut"
+
+a = input ("Ist Das Wetter Gut ?")
+
+if a == "Ja":
+    print ("Das ist gut")
+elif a == "Nein":
+    print ("Das ist schlecht")
+else:
+    print ("Fehler")    
+    
+    
