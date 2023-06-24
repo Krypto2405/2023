6d756b28acc2470250fe864ee6deed65cd8f132c
hallewest1 <larssonrecords@googlemail.com>
2023-06-24 14:27:17 +0200

    Changes to be committed: 	new file:   .vscode/tasks.json 	new file:   Projekte/Kris/C++/if-else.cpp 	new file:   Projekte/Kris/C++/random_num.cpp 	new file:   Projekte/Kris/C++/random_num.dSYM/Contents/Info.plist 	new file:   Projekte/Kris/C++/random_num.dSYM/Contents/Resources/DWARF/random_num 	renamed:    Projekte/Kris/Prof.py -> Projekte/Kris/Python/Prof.py 	renamed:    Projekte/Kris/WordlistGenerator.py -> Projekte/Kris/Python/WordlistGenerator.py 	renamed:    Projekte/Kris/temperatur_umwandler.py -> Projekte/Kris/Python/temperatur_umwandler.py

--- Projekte/Kris/Python/Prof.py
+++ Projekte/Kris/Python/Prof.py
@@ -24,13 +24,14 @@ Es gilt folgendes:
 Hinweis: Wie kann man auf halbe Noten runden? Die Funktion round() rundet auf ganze Noten, z.B. round(5.4) = 5 aber round(5.4*2) = 11… ;)
 """
 
-p = input("Prüfungsnote [von 1 bis 6 mit Halbpunkten]:")
+p = int(input("Prüfungsnote [von 1 bis 6 mit Halbpunkten]:"))
 a = input("Augenfarbe [dunkel=1, hell=0]:")
 f = input("Frisur [kurze Haare=1, lange Haare=0]:")
-w = input("Wetter [schön=1, nicht schön=0]:")
 
-if 
-
-
-
-
+if a + f == 2:
+    print(int(p * 1.1))
+elif a + f == 0:
+    print(int(p * 0.9))
+else:
+    print("Fehler")
+    
